6f7c36f315bc5d19b3c06437c59c5f4da4febc63
hussainabdillah <hussainkelarno@gmail.com>
2025-06-30 15:34:06 +0700

    fix: configuring middleware for admin

--- middleware.ts
+++ middleware.ts
@@ -2,21 +2,6 @@
 // https://next-auth.js.org/configuration/nextjs#middleware
 // https://nextjs.org/docs/app/building-your-application/routing/middleware
 
-// import NextAuth from 'next-auth';
-// import authConfig from './auth.config';
-
-// const { auth } = NextAuth(authConfig);
-
-// export default auth((req) => {
-//   if (!req.auth) {
-//     const url = req.url.replace(req.nextUrl.pathname, '/');
-//     return Response.redirect(url);
-//   }
-// });
-
-// export const config = { matcher: ['/dashboard/:path*'] };
-
-// middleware.ts
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 import { getToken } from 'next-auth/jwt'
@@ -28,12 +13,17 @@ export async function middleware(request: NextRequest) {
 
   // Jika user belum login dan mau ke /admin atau /dashboard
   if (!token && (pathname.startsWith('/admin') || pathname.startsWith('/dashboard'))) {
-    return NextResponse.redirect(new URL('/signin', request.url))
+    return NextResponse.redirect(new URL('/', request.url))
   }
 
   // Jika user adalah user biasa dan coba akses /admin
   if (token?.role === 'user' && pathname.startsWith('/admin')) {
     return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
+
+  // Jika admin mencoba akses /dashboard
+  if (token?.role === 'admin' && pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/admin', request.url))
   }
 
   return NextResponse.next()
