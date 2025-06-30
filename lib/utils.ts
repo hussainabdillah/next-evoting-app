6326f301dc8b08658918f0e6fbc5a283c0331b58
hussainabdillah <hussainkelarno@gmail.com>
2025-06-30 17:05:50 +0700

    fix: unresolved import from lib

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,32 +1,32 @@
 import { type ClassValue, clsx } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 import { Active, DataRef, Over } from '@dnd-kit/core';
-import { ColumnDragData } from '@/sections/kanban/board-column';
-import { TaskDragData } from '@/sections/kanban/task-card';
+// import { ColumnDragData } from '@/sections/kanban/board-column';
+// import { TaskDragData } from '@/sections/kanban/task-card';
 
-type DraggableData = ColumnDragData | TaskDragData;
+// type DraggableData = ColumnDragData | TaskDragData;
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function hasDraggableData<T extends Active | Over>(
-  entry: T | null | undefined
-): entry is T & {
-  data: DataRef<DraggableData>;
-} {
-  if (!entry) {
-    return false;
-  }
+// export function hasDraggableData<T extends Active | Over>(
+//   entry: T | null | undefined
+// ): entry is T & {
+//   data: DataRef<DraggableData>;
+// } {
+//   if (!entry) {
+//     return false;
+//   }
 
-  const data = entry.data.current;
+//   const data = entry.data.current;
 
-  if (data?.type === 'Column' || data?.type === 'Task') {
-    return true;
-  }
+//   if (data?.type === 'Column' || data?.type === 'Task') {
+//     return true;
+//   }
 
-  return false;
-}
+//   return false;
+// }
 
 export function formatBytes(
   bytes: number,
